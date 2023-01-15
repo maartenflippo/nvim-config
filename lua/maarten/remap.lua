56f771dd10db0e27e2f3e9ffe3ed4fb6f4407931
Maarten Flippo <maartenflippo@outlook.com>
2023-01-15 14:07:11 +0100

    Added format-on-save and shortcuts to navigate panes.

--- lua/maarten/remap.lua
+++ lua/maarten/remap.lua
@@ -1,3 +1,9 @@
 vim.g.mapleader = " ";
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Pane navigation
+vim.keymap.set("n", "<C-j>", "<C-W>j");
+vim.keymap.set("n", "<C-k>", "<C-W>k");
+vim.keymap.set("n", "<C-h>", "<C-W>h");
+vim.keymap.set("n", "<C-l>", "<C-W>l");
