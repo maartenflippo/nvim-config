afa5239c2b8b532ec2bb7149a94613699525580c
Maarten Flippo <maartenflippo@outlook.com>
2023-12-28 22:40:49 +0100

    Re-enable basic keymaps and options.

---
+++ lua/core/opts.lua
@@ -0,0 +1,9 @@
+local opts = vim.o
+
+opts.scrolloff = 5
+opts.termguicolors = true
+opts.number = true
+opts.relativenumber = true
+opts.expandtab = true
+opts.tabstop = 4
+opts.shiftwidth = 4
---
+++ lua/core/remap.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = " ";
+
+-- Open file navigation
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Pane navigation
+vim.keymap.set("n", "<C-j>", "<C-W>j");
+vim.keymap.set("n", "<C-k>", "<C-W>k");
+vim.keymap.set("n", "<C-h>", "<C-W>h");
+vim.keymap.set("n", "<C-l>", "<C-W>l");
