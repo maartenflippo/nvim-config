c158db1f71f18ba5fb8492a51e46af61703b715d
Maarten Flippo <maartenflippo@outlook.com>
2023-12-29 17:15:02 +0100

    Add shortcut for buffer navigation

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -4,7 +4,11 @@ vim.g.mapleader = " ";
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Pane navigation
-vim.keymap.set("n", "<C-j>", "<C-W>j");
-vim.keymap.set("n", "<C-k>", "<C-W>k");
-vim.keymap.set("n", "<C-h>", "<C-W>h");
-vim.keymap.set("n", "<C-l>", "<C-W>l");
+vim.keymap.set("n", "<C-S-j>", "<C-W>j");
+vim.keymap.set("n", "<C-S-k>", "<C-W>k");
+vim.keymap.set("n", "<C-S-h>", "<C-W>h");
+vim.keymap.set("n", "<C-S-l>", "<C-W>l");
+
+-- Buffer navigation
+vim.keymap.set("n", "<C-h>", vim.cmd.bnext)
+vim.keymap.set("n", "<C-l>", vim.cmd.bprevious)
