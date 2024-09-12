2a07cbadaa6bf7646ef4ab42b7cf7c25e9ab33bb
Maarten Flippo <maartenflippo@outlook.com>
2024-09-12 14:22:02 +0200

    Move lualine config to plugins folder

--- lua/presentation/init.lua
+++ lua/presentation/init.lua
@@ -1,16 +1,1 @@
 vim.cmd [[colorscheme tokyonight]]
-
-require('lualine').setup {
-    options = {
-        theme = 'tokyonight'
-    },
-
-    sections = {
-        lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics' },
-        lualine_c = { 'filename' },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location', require('lsp-progress').progress }
-    },
-}
