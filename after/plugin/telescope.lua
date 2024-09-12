9d993d4e952db5b77656d46c26b31c219d6edff2
Maarten Flippo <maartenflippo@outlook.com>
2024-09-12 10:47:29 +0200

    Consolidate configuration in plugins directory

--- after/plugin/telescope.lua
+++
@@ -1,5 +0,0 @@
-local builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fr", builtin.lsp_workspace_symbols, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,7 +2,7 @@ return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-        auto_open = true,
+        auto_open = false,
         auto_close = true,
     },
 }
